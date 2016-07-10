3f07866828fc633e157c5ed5d964ccd45c818d2b
Akexorcist <akexorcist@gmail.com>
2016-07-10 10:41:12 +0700

    Add presenter and view interface

--- app/src/main/java/com/akexorcist/mvpsimple/module/feed/FeedContractor.java
+++ app/src/main/java/com/akexorcist/mvpsimple/module/feed/FeedContractor.java
@@ -1,16 +1,22 @@
 package com.akexorcist.mvpsimple.module.feed;
 
+import com.akexorcist.mvpsimple.network.model.PostList;
+
 /**
  * Created by Akexorcist on 7/10/16 AD.
  */
 public class FeedContractor {
 
     public interface Presenter {
-
+        void loadPostList();
     }
 
     public interface View {
+        void updatePostList(PostList postList);
 
+        void showLoading();
+
+        void hideLoading();
     }
 
 }
