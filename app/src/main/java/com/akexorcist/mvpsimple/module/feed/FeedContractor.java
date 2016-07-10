12937d89c1fbfd5b43be263b5520f95edc74da8b
Akexorcist <akexorcist@gmail.com>
2016-07-10 10:04:48 +0700

    Init contractor

---
+++ app/src/main/java/com/akexorcist/mvpsimple/module/feed/FeedContractor.java
@@ -0,0 +1,16 @@
+package com.akexorcist.mvpsimple.module.feed;
+
+/**
+ * Created by Akexorcist on 7/10/16 AD.
+ */
+public class FeedContractor {
+
+    public interface Presenter {
+
+    }
+
+    public interface View {
+
+    }
+
+}
