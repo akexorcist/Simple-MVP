6e164e55ec43ba611716f0c9a698cab886fb8865
Akexorcist <akexorcist@gmail.com>
2016-07-10 11:10:36 +0700

    Create presenter

--- app/src/main/java/com/akexorcist/mvpsimple/module/feed/FeedActivity.java
+++ app/src/main/java/com/akexorcist/mvpsimple/module/feed/FeedActivity.java
@@ -1,16 +1,52 @@
 package com.akexorcist.mvpsimple.module.feed;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class FeedActivity extends AppCompatActivity {
+import com.akexorcist.mvpsimple.network.model.PostList;
+
+public class FeedActivity extends AppCompatActivity implements FeedContractor.View {
+
+    private FeedContractor.Presenter feedPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.akexorcist.mvpsimple.R.layout.activity_feed);
 
+        createPresenter();
+    }
 
+    private void createPresenter() {
+        FeedPresenter feedPresenter = new FeedPresenter(this);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        feedPresenter.start();
+
+    }
+
+    @Override
+    public void updatePostList(PostList postList) {
+
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    public void setPresenter(FeedContractor.Presenter presenter) {
+        this.feedPresenter = presenter;
+        Log.e("CHeck", "Set Presenter");
     }
 }
