f2d9edd0236072876dca76d2e60565f3883c22f2
Akexorcist <akexorcist@gmail.com>
2016-07-10 09:23:33 +0700

    Init commit

---
+++ app/src/main/java/com/akexorcist/mvpsimple/MainActivity.java
@@ -0,0 +1,16 @@
+package com.akexorcist.mvpsimple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.akexorcist.mvpsimple.R.layout.activity_main);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/akexorcist/mvpsimple/network/model/PostList.java
@@ -0,0 +1,238 @@
+package com.akexorcist.mvpsimple.network.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Akexorcist on 7/10/16 AD.
+ */
+
+
+public class PostList {
+    String kind;
+    String nextPageToken;
+
+    public class Item {
+        String kind;
+        String id;
+        Blog blog;
+        String published;
+        String updated;
+        String etag;
+        String url;
+        String selfLink;
+        String title;
+        @SerializedName("images")
+        List<Image> imageList;
+        Author author;
+        @SerializedName("replies")
+        Reply reply;
+        @SerializedName("labels")
+        List<String> labelList;
+
+        public Item() {
+        }
+
+        public String getKind() {
+            return kind;
+        }
+
+        public void setKind(String kind) {
+            this.kind = kind;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public Blog getBlog() {
+            return blog;
+        }
+
+        public void setBlog(Blog blog) {
+            this.blog = blog;
+        }
+
+        public String getPublished() {
+            return published;
+        }
+
+        public void setPublished(String published) {
+            this.published = published;
+        }
+
+        public String getUpdated() {
+            return updated;
+        }
+
+        public void setUpdated(String updated) {
+            this.updated = updated;
+        }
+
+        public String getEtag() {
+            return etag;
+        }
+
+        public void setEtag(String etag) {
+            this.etag = etag;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getSelfLink() {
+            return selfLink;
+        }
+
+        public void setSelfLink(String selfLink) {
+            this.selfLink = selfLink;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public List<Image> getImageList() {
+            return imageList;
+        }
+
+        public void setImageList(List<Image> imageList) {
+            this.imageList = imageList;
+        }
+
+        public Author getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(Author author) {
+            this.author = author;
+        }
+
+        public Reply getReply() {
+            return reply;
+        }
+
+        public void setReply(Reply reply) {
+            this.reply = reply;
+        }
+
+        public List<String> getLabelList() {
+            return labelList;
+        }
+
+        public void setLabelList(List<String> labelList) {
+            this.labelList = labelList;
+        }
+    }
+
+    public class Blog {
+        String id;
+
+        public Blog() {
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+    }
+
+    public class Image {
+        String url;
+
+        public Image() {
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+
+    public class Author {
+        String id;
+        String displayName;
+        String url;
+        @SerializedName("images")
+        List<Image> imageList;
+
+        public Author() {
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getDisplayName() {
+            return displayName;
+        }
+
+        public void setDisplayName(String displayName) {
+            this.displayName = displayName;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public List<Image> getImageList() {
+            return imageList;
+        }
+
+        public void setImageList(List<Image> imageList) {
+            this.imageList = imageList;
+        }
+    }
+
+    public class Reply {
+        String totalItems;
+        String selfLink;
+
+        public Reply() {
+        }
+
+        public String getTotalItems() {
+            return totalItems;
+        }
+
+        public void setTotalItems(String totalItems) {
+            this.totalItems = totalItems;
+        }
+
+        public String getSelfLink() {
+            return selfLink;
+        }
+
+        public void setSelfLink(String selfLink) {
+            this.selfLink = selfLink;
+        }
+    }
+}
