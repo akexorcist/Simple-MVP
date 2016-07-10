b78d36ee4f1c4c93dfc0fc767981a5983c52f071
Akexorcist <akexorcist@gmail.com>
2016-07-10 10:04:01 +0700

    Change activity name

--- app/src/main/java/com/akexorcist/mvpsimple/module/feed/FeedActivity.java
+++ app/src/main/java/com/akexorcist/mvpsimple/module/feed/FeedActivity.java
@@ -1,14 +1,14 @@
-package com.akexorcist.mvpsimple;
+package com.akexorcist.mvpsimple.module.feed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.akexorcist.mvpsimple.R.layout.activity_main);
+        setContentView(com.akexorcist.mvpsimple.R.layout.activity_feed);
 
 
 
