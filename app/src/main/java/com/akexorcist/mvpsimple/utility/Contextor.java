47a23766fdc5ec506358b3df559c6741d130ff73
Akexorcist <akexorcist@gmail.com>
2016-07-14 00:19:45 +0700

    Create Contextor

---
+++ app/src/main/java/com/akexorcist/mvpsimple/utility/Contextor.java
@@ -0,0 +1,27 @@
+package com.akexorcist.mvpsimple.utility;
+
+import android.content.Context;
+
+/**
+ * Created by Akexorcist on 7/14/16 AD.
+ */
+public class Contextor {
+    private static Contextor contextor;
+
+    public static Contextor getInstance() {
+        if (contextor == null) {
+            contextor = new Contextor();
+        }
+        return contextor;
+    }
+
+    public Context context;
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+}
