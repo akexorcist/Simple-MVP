88a072f112e6d5a6da8c86bb94a118b100620a84
Akexorcist <akexorcist@gmail.com>
2016-07-29 05:04:28 +0700

    Rename fragment_viewer_image_header to fragment_viewer_header

--- app/src/main/java/com/akexorcist/mvpsimple/module/viewer/header/FeedHeaderFragment.java
+++ app/src/main/java/com/akexorcist/mvpsimple/module/viewer/header/FeedHeaderFragment.java
@@ -35,7 +35,7 @@ public class FeedHeaderFragment extends Fragment implements FeedHeaderContractor
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_viewer_image_header, container, false);
+        return inflater.inflate(R.layout.fragment_viewer_header, container, false);
     }
 
     @Override
